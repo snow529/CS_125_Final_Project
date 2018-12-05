7f8add178b5bacff742b301065813efbba13ef8d
Snowdream <yzy1993@gmail.com>
2018-12-05 01:06:25 -0600

    a working second screen

---
+++ app/src/main/java/com/example/finalproject/myapplication/Activity1.java
@@ -0,0 +1,24 @@
+package com.example.finalproject.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity1 extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+        Button next = findViewById(R.id.button02);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        });
+    }
+}
