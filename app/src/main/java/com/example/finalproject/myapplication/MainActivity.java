fa3ef5f1912af99d719ebf29dc45e385b14cd6c7
Snowdream <yzy1993@gmail.com>
2018-12-05 03:33:44 -0600

    change of name

--- app/src/main/java/com/example/finalproject/myapplication/MainActivity.java
+++ app/src/main/java/com/example/finalproject/myapplication/MainActivity.java
@@ -5,8 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    RatingBar strBar = (RatingBar) findViewById(R.id.ratingBar01);
+    RatingBar agiBar = (RatingBar) findViewById(R.id.ratingBar02);
+    RatingBar itlBar = (RatingBar) findViewById(R.id.ratingBar03);
+    RatingBar spdBar = (RatingBar) findViewById(R.id.ratingBar04);
+    RatingBar tncBar = (RatingBar) findViewById(R.id.ratingBar05);
+    */
+
+    /*
+    private int str = (int) strBar.getRating();
+    private int agi = (int) agiBar.getRating();
+    private int itl = (int) itlBar.getRating();
+    private int spd = (int) spdBar.getRating();
+    private int tnc = (int) tncBar.getRating();
+
+    public int[] getStats() {
+        int[] stats = {str, agi, itl, spd, tnc};
+        return stats;
+    }
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/finalproject/myapplication/Result.java
+++ app/src/main/java/com/example/finalproject/myapplication/Result.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        ((TextView) findViewById(R.id.textView11)).setText("ratingbar code always crashes, please send help ._.");
+        //int[] intResult ={(int) strBar.getRating(), (int) agiBar.getRating(), (int) itlBar.getRating(), (int) spdBar.getRating(), (int) tncBar.getRating()};
+        //String result = "Return some results with strength at " + intResult[0] + ", agility at " + intResult[1] + ", intellect at " + intResult[2] + ", speed at " + intResult[3] + ", and tenacity at " + intResult[4];
+
+
 
         Button next = findViewById(R.id.button02);
         next.setOnClickListener(new View.OnClickListener() {
@@ -20,5 +28,6 @@ public class Result extends AppCompatActivity {
             }
 
         });
+
     }
 }
